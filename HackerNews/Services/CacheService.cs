79965aa1b11978e7c7ea41abf1d315794c85be7c
Thomas French <frenche6@gmail.com>
2020-08-21 12:20:00 -0400

    Added lastUpdated to cache

--- HackerNews/Services/CacheService.cs
+++ HackerNews/Services/CacheService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using HackerNews.Models;
 using HackerNews.Services.Interfaces;
 
@@ -7,6 +8,7 @@ namespace HackerNews.Services
     public class CacheService : ICacheService
     {
         private List<StoryItem> _newStoryItems;
+        private DateTime _lastUpdated;
 
         public CacheService()
         {
@@ -15,12 +17,21 @@ namespace HackerNews.Services
 
         public List<StoryItem> GetNewStories()
         {
+            if (_lastUpdated < DateTime.UtcNow.AddMinutes(-5))
+                ClearCache();
+
             return _newStoryItems.Count > 0 ? _newStoryItems : null;
         }
         
         public void SetNewStories(List<StoryItem> storyItems)
         {
             _newStoryItems = storyItems;
+            _lastUpdated = DateTime.UtcNow;
+        }
+
+        private void ClearCache()
+        {
+            _newStoryItems.Clear();
         }
     }
 }
