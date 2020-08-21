4b16f6a015d16113508e54ba42ed57612d940dfd
Thomas French <frenche6@gmail.com>
2020-08-21 12:31:28 -0400

    Added health controller

---
+++ HackerNews/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNews.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : Controller
+    {
+        // GET
+        [HttpGet]
+        [Route("Health")]
+        public ActionResult<string> Health()
+        {
+            return "Status ok - " + DateTime.UtcNow.ToLongDateString();
+        }
+    }
+}
